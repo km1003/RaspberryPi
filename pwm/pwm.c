45ec5297d80a50e369802d4af5e5238d2570f453
km1003 <km1003@gmail.com>
2012-10-26 11:54:59 -0500

    updated with TODO's for motor controller

--- pwm/pwm.c
+++ pwm/pwm.c
@@ -6,8 +6,34 @@
 
 int main(int argc, char** argv)
 {
+  int i;
+
+  // Setup wiringPi to use GPIO mode
   if (wiringPiSetupGpio () == -1)
     exit (1) ;
+
+  pinMode(18, PWM_OUTPUT);
+
+  //TODO: Enable level shifter
+  //      Set motor polarity pins
+
+  for(;;)
+  {
+    // Speed up
+    for(i = 0; i < 1024; i++)
+    {
+      pwmWrite(18, i);
+      delay(10);
+    }
+
+    // Slow down
+    for(i = 1023; i >= 0; i--)
+    {
+      pwmWrite(18, i);
+      delay(10);
+    }
+
+  }
 
   return 0;
 }
