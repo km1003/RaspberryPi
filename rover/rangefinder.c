eed99dc04ead48d74a4b2db139dc27e29f84fda5
km1003 <km1003@gmail.com>
2012-11-03 00:52:23 -0500

    added rover driving code

--- rover/rangefinder.c
+++ rover/rangefinder.c
@@ -1,6 +1,7 @@
 /*
  * rangefinder.c for the URM37 ultrasonic rangefinder used with
  * the Raspberry Pi
+ * TODO: use a wiringPi interrupt thread to read the range
  */
 #include <stdio.h>
 #include <wiringPi.h>
@@ -13,6 +14,7 @@
 int uart;
 char autoModeCmd[] = {0x44, 0x02, 0xaa, 0xf0};
 
+// set the rangefinder mode and gpio pins to read
 int initRangefinder()
 {
   if(wiringPiSetupGpio() == -1)
@@ -35,21 +37,23 @@ int initRangefinder()
   // start the rangefinder in autonomous mode
   writeSerial(autoModeCmd, 4, uart);
 
-  // allow time for the rangefinder to start up
-  delay(300);
-
   return 0;
 }
 
 // get the range from the pwm out
 int getRange()
 {
-  uint64_t start=0, stop=0, pulseWidth;
-  while(digitalRead(PWMIN) != 1);
-  while(digitalRead(PWMIN) == 1);
-  start = getuSecs();
-  while(digitalRead(PWMIN) == 0);
-  stop = getuSecs();
-  pulseWidth = stop - start;
-  return (int)pulseWidth/50;
+  int range;
+  uint64_t start=0, stop=0, pulseWidth;	
+  do
+  {
+    while(digitalRead(PWMIN) == LOW);	// wait for high signal
+    while(digitalRead(PWMIN) == HIGH);	// wait for falling edge
+    start = getuSecs();			// start of the low pulse
+    while(digitalRead(PWMIN) == LOW);	// wait for the end of the pulse
+    stop = getuSecs();			// end of the low pulse
+    pulseWidth = stop - start;		// length of pulse in uSecs
+    range=(int)((pulseWidth/50.0)+0.5);	// every 50us of pulse = 1cm
+  } while(range < 3 || range > 600);	// valid range is 1cm - 500cm
+  return range;
 }
--- rover/rover.c
+++ rover/rover.c
@@ -1,3 +1,4 @@
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -6,6 +7,7 @@
 #include "compass.h"
 #include "rangefinder.h"
 #include "serial.h"
+#include "time.h"
 
 // Defines
 #define FALSE   (0)
@@ -19,29 +21,58 @@
 #define PWM     (18)
 #define M2POL   (27)
 
+// Enumerations
+typedef enum
+{
+  NONE		= 0,
+  FORWARD	= 1,
+  REVERSE	= 2,
+  LEFT		= 3,
+  RIGHT		= 4,
+} Direction;
+
 // Type definitions
 typedef char    bool;
+typedef struct
+{
+  bool		isDriving;
+  Direction	direction;
+  Direction	lastTurn;
+  int		speed;
+  int		range;
+} Rover;
 
 // Function prototypes
+void processMove(void);
+void toString(void);
 void init(void);
-void turnLeft(int speed);
-void turnRight(int speed);
-void driveForward(int speed);
-void driveReverse(int speed);
+void drive(Direction direction, int speed);
 void stop(void);
+void signalHandler(int sig);
 
+// Globals
+Rover		rover;
+	
 int main(int argc, char** argv)
 {
-  int i;
   init();
-
+  int minR=0, maxR=0, minT=0, maxT=0, dif, range;
+  uint64_t start, end;
   while(1)
   {
-    printf("range: %dcm\n", getRange());
+    start = getuSecs();
+    range = getRange();
+    end = getuSecs();
+    dif = (int)(end-start);
+    if(range < minR || minR==0) minR=range;
+    if(range > maxR) maxR=range;
+    if(dif < minT || minT==0) minT=dif;
+    if(dif > maxT) maxT=dif;
+    printf("range: %dcm min: %dcm max: %dcm\n", range, minR, maxR);
+    printf("elapsed: %dus min: %dus max: %dus\n", dif, minT, maxT);
     delay(100);
   }
-
-/* TODO: work on sensor code
+/* 
   // drive in a ccw square
   for(i = 0; i < 10; i++)
   {
@@ -50,17 +81,111 @@ int main(int argc, char** argv)
     turnLeft(512);
     delay(300);
   }
-  
-  stop();
+  stop();  
 */
+/*
+  // Main Loop
+  while(1)
+  {
+    // get range infront of the rover
+    rover.range = getRange();
 
+    // process next move
+    processMove();
 
+    // print rover state to screen
+    toString();
 
+    // sleep thread a while
+    delay(50);
+  }
+*/
   return 0;
+}
+
+// Process the next move for the rover to make
+void processMove()
+{
+  // an is obsticle near
+  if(rover.range < 25)
+  {
+    // rover is driving, stop to avoid a collision
+    if(rover.isDriving && rover.direction == FORWARD)
+    {
+      stop();
+      printf("\nobsticle near, stopping\n");
+    }
+    // rover is stopped, rotate right or left to look for a clear path
+    if(!rover.isDriving)
+    {
+      // enough room to rotate
+      if(rover.range > 15)
+      {
+        if(rover.lastTurn == LEFT) drive(RIGHT, rover.speed);
+        else if(rover.lastTurn == RIGHT) drive(LEFT, rover.speed);
+        else drive(LEFT, rover.speed);
+        printf("\nrotating to a new heading\n");
+      }
+      // not enough room to rotate so reverse
+      else
+      {
+        drive(REVERSE, rover.speed);
+        printf("\nobsticle very close, reversing\n");
+      }
+    }
+  }
+  
+  // resume driving when obsticle is clear
+  if(rover.range > 35)
+  {
+    if(!rover.isDriving)
+    {
+      drive(FORWARD, rover.speed);
+      printf("\nresume driving forward\n");
+    }
+    if(rover.isDriving)
+    {
+      if(rover.direction == RIGHT || rover.direction == LEFT)
+      {
+        stop();
+        printf("\nfound a clear heading\n");
+      }
+      if(rover.direction == REVERSE)
+      {
+        stop();
+        printf("\nreversed to a safe distance\n");
+      }
+    }
+  }
+}
+
+// Print rover state to stdout
+void toString()
+{
+  // print status to stdout
+  char* mving=(char*)"stopped";
+  char* dir=(char*)" ";
+  if(rover.isDriving)
+  {
+    mving = "driving";
+    if(rover.direction==FORWARD)
+      dir = "forward";
+    if(rover.direction==REVERSE)
+      dir = "reverse";
+    if(rover.direction==LEFT)
+      dir = "left";
+    if(rover.direction==RIGHT)
+      dir = "right";
+  }
+  printf("\r%s %s at %d, range: %d                               ",
+          mving, dir, rover.speed, rover.range);
 }
 
 void init()
 {
+  // handle CNTRL+C signal
+  signal(SIGINT, &signalHandler);
+
   // Setup wiringPi to use GPIO mode
   if (wiringPiSetupGpio () == -1)
     exit (1) ;
@@ -73,37 +198,58 @@ void init()
   pwmWrite(PWM, 0);		// zero pwm output
   if(initRangefinder() < 0)
     printf("failed to init rangefinder\n");
+  stop();
+  rover.isDriving = FALSE;
+  rover.direction = NONE;
+  rover.lastTurn = NONE;
+  rover.speed = 512;
+  rover.range = 0;
 }
 
-void turnLeft(int speed)
+void drive(Direction direction, int speed)
 {
-  digitalWrite(M1POL, 1);
-  digitalWrite(M2POL, 1);
+  int m1p = LOW, m2p = LOW;
+  switch(direction)
+  {
+    case FORWARD:
+      m1p = LOW;
+      m2p = HIGH;
+      break;
+    case REVERSE:
+      m1p = HIGH;
+      m2p = LOW;
+      break;
+    case LEFT:
+      m1p = HIGH;
+      m2p = HIGH;
+      rover.lastTurn = LEFT;
+      break;
+    case RIGHT:
+      m1p = LOW;
+      m2p = LOW;
+      rover.lastTurn = RIGHT;
+      break;
+    case NONE:
+    default:
+      return;
+  }
+  digitalWrite(M1POL, m1p);
+  digitalWrite(M2POL, m2p);
   pwmWrite(PWM, speed);
-}
-
-void turnRight(int speed)
-{
-  digitalWrite(M1POL, 0);
-  digitalWrite(M2POL, 0);
-  pwmWrite(PWM, speed);
-}
-
-void driveForward(int speed)
-{
-  digitalWrite(M1POL, 0);
-  digitalWrite(M2POL, 1);
-  pwmWrite(PWM, speed);
-}
-
-void driveReverse(int speed)
-{
-  digitalWrite(M1POL, 1);
-  digitalWrite(M2POL, 0);
-  pwmWrite(PWM, speed);
+  rover.isDriving = TRUE;
+  rover.direction = direction;
 }
 
 void stop()
 {
   pwmWrite(PWM, 0);
+  rover.isDriving = FALSE;
+}
+
+// Handle OS signals
+void signalHandler(int sig)
+{
+  printf("\nControl+C received, shutting down\n");
+  stop();	// stop the rover
+  exit(0);	// exit the program
 }
